f4811ff6f2805404e242c33cf24ed6b5ab848eef
Android <110397800+Celsooz@users.noreply.github.com>
2022-10-19 15:34:15 -0300

    tipo

---
+++ Tipos de Variáveis/Tipos de Variáveis/Calculadora.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tipos_de_Variáveis
+{
+    class Calculadora 
+    {
+
+       public void somar()
+        {
+            int num1 = 5;
+            int num2 = 6;
+            int resultado = num1 + num2;
+            Console.WriteLine("O resultado é: {0}", resultado.ToString());
+        }
+    }
+}
