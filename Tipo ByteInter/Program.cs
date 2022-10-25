54ceac173a61f1170d8b6248991a0c7bde02d5e0
Android <110397800+Celsooz@users.noreply.github.com>
2022-10-25 10:04:13 -0300

    byte

---
+++ Tipo ByteInter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Tipo_ByteInter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte v1 = 1;
+            Console.WriteLine("O valor de V1 é: {0}",v1);
+            Console.WriteLine("O tipo é: {0}", v1.GetType());
+            
+            Console.WriteLine("Digite um valor na faixa de 0 a 255");
+            try
+            {
+                byte vdigitado = Convert.ToByte(Console.ReadLine());
+                Console.WriteLine("O valor digitado é byte");
+
+            }
+            catch (OverflowException) 
+            {
+                Console.WriteLine("O valor digitado não é byte ");
+               
+            }
+
+
+
+
+        }
+    }
+}
---
+++ Tipos de Variáveis/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Tipos_de_Variáveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = num1;
+            int resultado = num1 + num2;
+            num1 = 6;
+            Console.WriteLine("Valor de num1 é: {0} e está na stack", num1.ToString());
+            Console.WriteLine("Valor de num2 é: {0} e está na stack", num2.ToString());
+
+            Console.WriteLine("Calculadora");
+            Calculadora minhacalculadora = new Calculadora();
+            minhacalculadora.somar();
+
+            Console.WriteLine("Variáveis Nulla");
+            int?  altura = null;
+
+            if (altura.HasValue)
+            {
+                Console.WriteLine("O valor é: {0}:", altura);
+
+            }
+            else
+            {
+                Console.WriteLine("Altura está nullo");
+
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
